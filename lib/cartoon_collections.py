d10dd4789844fce8eadc2df936a5c8e959920ddb
Davit Tabidze <davidtabidzef1@gmail.com>
2023-07-05 14:17:58 -0400

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
+def roll_call_dwarves(names):
+    for i in range (len(names)):
+        print (f"{i+1}. {names[i]}")
     pass
 
-def summon_captain_planet():
+def summon_captain_planet(planeteers):
+    return [planeter.capitalize()+'!' for planeter in planeteers]
     pass
 
-def long_planeteer_calls():
+def long_planeteer_calls(planeteers):
+    for planeteer in planeteers:
+        if len(planeteer) > 4:
+            return True
+    return False
     pass
 
-def find_the_cheese():
+def find_the_cheese(ingredients):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for ingredient in ingredients:
+        if ingredient in cheeses:
+            return ingredient
+    return None
     pass
